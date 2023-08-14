0c971288fa2f52664aa95d3e554f6502581f6e8e
setix-studio <setix.studio@gmail.com>
2023-08-13 22:36:24 -0500

    updating files

--- TurretDefense/main.py
+++ TurretDefense/main.py
@@ -27,43 +27,43 @@ selected_turret = None
 #load images
 
 #map
-map_image = pg.image.load('levels/level.png').convert_alpha()
+map_image = pg.image.load('TurretDefense/levels/level.png').convert_alpha()
 #turret spritesheets
 turret_spritesheets = []
 for x in range(1, c.TURRET_LEVELS + 1):
-    turret_sheet = pg.image.load(f'assets/images/turrets/turret_{x}.png').convert_alpha()
+    turret_sheet = pg.image.load(f'TurretDefense/assets/images/turrets/turret_{x}.png').convert_alpha()
     turret_spritesheets.append(turret_sheet)
 #individual turret image for mouse cursor
-cursor_turret = pg.image.load('assets/images/turrets/cursor_turret.png').convert_alpha()
+cursor_turret = pg.image.load('TurretDefense/assets/images/turrets/cursor_turret.png').convert_alpha()
 #enemies
 enemy_images = {
-    "weak" : pg.image.load('assets/images/enemies/enemy_1.png').convert_alpha(),
-    "medium" : pg.image.load('assets/images/enemies/enemy_2.png').convert_alpha(),
-    "strong" : pg.image.load('assets/images/enemies/enemy_3.png').convert_alpha(),
-    "elite" : pg.image.load('assets/images/enemies/enemy_4.png').convert_alpha()
+    "weak" : pg.image.load('TurretDefense/assets/images/enemies/enemy_1.png').convert_alpha(),
+    "medium" : pg.image.load('TurretDefense/assets/images/enemies/enemy_2.png').convert_alpha(),
+    "strong" : pg.image.load('TurretDefense/assets/images/enemies/enemy_3.png').convert_alpha(),
+    "elite" : pg.image.load('TurretDefense/assets/images/enemies/enemy_4.png').convert_alpha()
 }
 #buttons
-buy_turret_image = pg.image.load('assets/images/buttons/buy_turret.png').convert_alpha()
-cancel_turret_image = pg.image.load('assets/images/buttons/cancel.png').convert_alpha()
-upgrade_turret_image = pg.image.load('assets/images/buttons/upgrade_turret.png').convert_alpha()
-begin_image = pg.image.load('assets/images/buttons/begin.png').convert_alpha()
-restart_image = pg.image.load('assets/images/buttons/restart.png').convert_alpha()
-fast_forward_image = pg.image.load('assets/images/buttons/fast_forward.png').convert_alpha()
+buy_turret_image = pg.image.load('TurretDefense/assets/images/buttons/buy_turret.png').convert_alpha()
+cancel_turret_image = pg.image.load('TurretDefense/assets/images/buttons/cancel.png').convert_alpha()
+upgrade_turret_image = pg.image.load('TurretDefense/assets/images/buttons/upgrade_turret.png').convert_alpha()
+begin_image = pg.image.load('TurretDefense/assets/images/buttons/begin.png').convert_alpha()
+restart_image = pg.image.load('TurretDefense/assets/images/buttons/restart.png').convert_alpha()
+fast_forward_image = pg.image.load('TurretDefense/assets/images/buttons/fast_forward.png').convert_alpha()
 #gui
-heart_image = pg.image.load('assets/images/gui/heart.png').convert_alpha()
-coin_image = pg.image.load('assets/images/gui/coin.png').convert_alpha()
-logo_image = pg.image.load('assets/images/gui/logo.png').convert_alpha()
+heart_image = pg.image.load('TurretDefense/assets/images/gui/heart.png').convert_alpha()
+coin_image = pg.image.load('TurretDefense/assets/images/gui/coin.png').convert_alpha()
+logo_image = pg.image.load('TurretDefense/assets/images/gui/logo.png').convert_alpha()
 
 #icon
 pg.display.set_icon(coin_image) #update later!
 
 #load sounds
-shot_fx = pg.mixer.Sound('assets/audio/shot.wav')
+shot_fx = pg.mixer.Sound('TurretDefense/assets/audio/shot.wav')
 shot_fx.set_volume(0.5)
 
 #load json for level
-with open('levels/level.tmj') as file:d
-world_data = json.load(file)
+with open('TurretDefense/levels/level.tmj') as file:
+    world_data = json.load(file)
 
 #load fonts
 text_font = pg.font.SysFont("Consolas", 24, True)
